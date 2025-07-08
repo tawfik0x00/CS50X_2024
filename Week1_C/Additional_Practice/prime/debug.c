18237713affc6aadd6486c75812ec6e9f9062a42
Mohamed Tawfik <tawfik0x00@users.noreply.github.com>
2025-07-08 17:49:24 +0300

    Week1 Additional Practice

---
+++ Week1_C/Additional_Practice/prime/debug.c
@@ -0,0 +1,52 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool prime(int number);
+
+int main(void)
+{
+    // get the minimum number to start
+    int min;
+    do
+    {
+        min = get_int("Minimum: ");
+    }
+    while (min < 1);
+
+    // get the max to end
+    int max;
+    do
+    {
+        max = get_int("Maximum: ");
+    }
+    while (min >= max);
+
+
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
+        }
+    }
+}
+
+bool prime(int number)
+{
+    if (number == 1)
+    {
+        return false;
+    }
+    
+    for (int factor = 2; factor < number; factor++)
+    {
+        // if we find any number give remainder of 0 mean this number divisble by number
+        // other than 1 and itself because we start at 2 and end before the number.
+        if (number % factor == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
